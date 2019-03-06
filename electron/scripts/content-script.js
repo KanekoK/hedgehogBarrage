84805f4a8c565e7547564db76cbd132d798899b3
kaneko <b1r2d3q4@gmail.com>
2019-03-06 15:47:09 +0900

    .

--- electron/scripts/content-script.js
+++ electron/scripts/content-script.js
@@ -3,8 +3,8 @@
 (function () {
 
   // change to your server url
-  // const SERVER_URL = 'https://hedgehogs.site'
-  const SERVER_URL = 'http://localhost:2525/'
+  const SERVER_URL = 'https://hedgehogs.site'
+  // const SERVER_URL = 'http://localhost:2525/'
 
   // const APP_ID = chrome.runtime.id
   // const APP_VERSION = chrome.runtime.getManifest().version
